90547b3b6bd34a7febb2bda4e10a358b30a49f4b
waffy1901 <waffyahmed@gmail.com>
2023-07-10 15:06:49 -0400

    UI changes

--- app/src/main/java/com/example/jobsearchapplication/MainActivity.kt
+++ app/src/main/java/com/example/jobsearchapplication/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.jobsearchapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.w3c.dom.Text
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+//    // get reference to button
+//    val resourcesBtn = findViewById(R.id.resources) as Button
+//    // set on-click listener
+//    fun show() {
+//        resourcesBtn.setOnClickListener {
+//            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+//        }
+//    }
 
 }
