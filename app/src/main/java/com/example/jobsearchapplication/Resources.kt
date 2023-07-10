268ffc03db38c12eee15790b2ce12bddadb01353
waffy1901 <waffyahmed@gmail.com>
2023-07-10 17:49:05 -0400

    Added functionality for switching between screens

--- app/src/main/java/com/example/jobsearchapplication/Resources.kt
+++ app/src/main/java/com/example/jobsearchapplication/Resources.kt
@@ -1,16 +1,16 @@
 package com.example.jobsearchapplication
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class Resources : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val resourcesBtn = findViewById(R.id.resources) as Button
+        setContentView(R.layout.resource)
+        val resourcesBtn = findViewById(R.id.home) as Button
         resourcesBtn.setOnClickListener {
-            val intent = Intent(this, Resources::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
