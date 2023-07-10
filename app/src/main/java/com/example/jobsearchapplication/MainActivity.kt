433256a5ae6c6dd228ced5ed84de3cc4790cb7ed
waffy1901 <waffyahmed@gmail.com>
2023-07-10 15:04:16 -0400

    UI portion started

--- app/src/main/java/com/example/jobsearchapplication/MainActivity.kt
+++ app/src/main/java/com/example/jobsearchapplication/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.jobsearchapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
