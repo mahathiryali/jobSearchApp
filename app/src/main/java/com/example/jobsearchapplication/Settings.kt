f8b19a05290e472d20d5db6a59f359a9389301f7
waffy1901 <waffyahmed@gmail.com>
2024-06-30 17:11:26 -0400

    Finished UI of settings page and created/finished the UI of the CreateAccount page

--- app/src/main/java/com/example/jobsearchapplication/Settings.kt
+++ app/src/main/java/com/example/jobsearchapplication/Settings.kt
@@ -3,7 +3,9 @@ package com.example.jobsearchapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 import android.widget.Button
+import android.widget.TextView
 
 class Settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,13 @@ class Settings : AppCompatActivity() {
         val homeBtn = findViewById<Button>(R.id.homePage)
         homeBtn.setOnClickListener {
             val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
+        val createAccountText = findViewById<TextView>(R.id.createAccountText)
+        createAccountText.text = Html.fromHtml("<u>Create Account</u>")
+        createAccountText.setOnClickListener {
+            val intent = Intent(this, CreateAccount::class.java)
             startActivity(intent)
         }
     }
