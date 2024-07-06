2b672c673e08c0a1151fd674372afd64c5e45bf9
waffy1901 <waffyahmed@gmail.com>
2024-07-06 15:07:51 -0400

    Added the ability to delete jobs from a user's profile and remedied some spacing issues

--- app/src/main/java/com/example/jobsearchapplication/JobAdapter.kt
+++ app/src/main/java/com/example/jobsearchapplication/JobAdapter.kt
@@ -7,15 +7,16 @@ import android.widget.TextView
 import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 
-class JobAdapter(private var jobs: List<Job>, private val showSaveButton: Boolean) : RecyclerView.Adapter<JobAdapter.JobViewHolder>() {
-
+class JobAdapter(private var jobs: List<Job>, private val showSaveButton: Boolean, private val showDeleteButton: Boolean) : RecyclerView.Adapter<JobAdapter.JobViewHolder>() {
     var onSaveJobClickListener: ((Job) -> Unit)? = null
+    var onDeleteJobClickListener: ((Job) -> Unit)? = null
 
     class JobViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val jobTitle: TextView = itemView.findViewById(R.id.jobTitle)
         val jobCompany: TextView = itemView.findViewById(R.id.jobCompany)
         val jobLocation: TextView = itemView.findViewById(R.id.jobLocation)
         val saveJobBtn: Button = itemView.findViewById(R.id.saveJobBtn)
+        val deleteJobBtn: Button = itemView.findViewById(R.id.deleteJobBtn)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JobViewHolder {
@@ -28,9 +29,15 @@ class JobAdapter(private var jobs: List<Job>, private val showSaveButton: Boolea
         holder.jobTitle.text = job.title
         holder.jobCompany.text = job.company.display_name
         holder.jobLocation.text = job.location.display_name
+
         holder.saveJobBtn.visibility = if (showSaveButton) View.VISIBLE else View.GONE
         holder.saveJobBtn.setOnClickListener {
             onSaveJobClickListener?.invoke(job)
+        }
+
+        holder.deleteJobBtn.visibility = if (showDeleteButton) View.VISIBLE else View.GONE
+        holder.deleteJobBtn.setOnClickListener {
+            onDeleteJobClickListener?.invoke(job)
         }
     }
 
